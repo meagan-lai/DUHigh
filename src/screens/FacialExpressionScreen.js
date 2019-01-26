f752638b675ff62309faab71b1a41d992ee09512
Meagan Lai <laimeagan9@gmail.com>
2019-01-26 18:43:59 -0500

    Image preview

--- src/screens/FacialExpressionScreen.js
+++ src/screens/FacialExpressionScreen.js
@@ -1,49 +1,87 @@
 import React from "react";
-import { Button, View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  Button,
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  Dimensions
+} from "react-native";
 import { RNCamera } from "react-native-camera";
 
 export default class FacialExpressionScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      imageUri: null
+    };
+  }
+
   takePicture = async function() {
     if (this.camera) {
       const options = { quality: 0.5, base64: true };
       const data = await this.camera.takePictureAsync(options);
+      this.setState({ imageUri: data.uri });
       console.log(data.uri);
     }
   };
 
-  render() {
+  renderCamera() {
     return (
-      <View style={styles.container}>
-        <Text>FFacial Expresstion Screen</Text>
-        <Button
-          title="Go to Pupil Screen"
-          onPress={() => this.props.navigation.navigate("Pupil")}
-        />
-        <RNCamera
-          ref={ref => {
-            this.camera = ref;
-          }}
-          style={styles.preview}
-          type={RNCamera.Constants.Type.back}
-          flashMode={RNCamera.Constants.FlashMode.on}
-          permissionDialogTitle={"Permission to use camera"}
-          permissionDialogMessage={
-            "We need your permission to use your camera phone"
-          }
-          onGoogleVisionBarcodesDetected={({ barcodes }) => {
-            console.log(barcodes);
-          }}
-        />
+      <RNCamera
+        ref={ref => {
+          this.camera = ref;
+        }}
+        style={styles.preview}
+        type={RNCamera.Constants.Type.back}
+        flashMode={RNCamera.Constants.FlashMode.off}
+        permissionDialogTitle={"Permission to use camera"}
+        permissionDialogMessage={
+          "We need your permission to use your camera phone"
+        }
+      >
         <View
           style={{ flex: 0, flexDirection: "row", justifyContent: "center" }}
         >
           <TouchableOpacity
             onPress={this.takePicture.bind(this)}
             style={styles.capture}
-          >
-            <Text style={{ fontSize: 14 }}> SNAP </Text>
-          </TouchableOpacity>
+          />
         </View>
+      </RNCamera>
+    );
+  }
+
+  renderImage() {
+    return (
+      <View>
+        <Image source={{ uri: this.state.imageUri }} style={styles.preview} />
+        <Text
+          style={styles.cancel}
+          onPress={() => this.setState({ imageUri: null })}
+        >
+          Cancel
+        </Text>
+        <Text
+          style={styles.next}
+          onPress={() => this.props.navigation.navigate("Pupil")}
+        >
+          Next
+        </Text>
+      </View>
+    );
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        {this.state.imageUri ? this.renderImage() : this.renderCamera()}
+        <Text>Facial Expresstion Screen</Text>
+        <Button
+          title="Go to Pupil Screen"
+          onPress={() => this.props.navigation.navigate("Pupil")}
+        />
       </View>
     );
   }
@@ -52,21 +90,43 @@ export default class FacialExpressionScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "column",
-    backgroundColor: "black"
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#000"
   },
   preview: {
     flex: 1,
     justifyContent: "flex-end",
-    alignItems: "center"
+    alignItems: "center",
+    height: Dimensions.get("window").height,
+    width: Dimensions.get("window").width
   },
   capture: {
-    flex: 0,
-    backgroundColor: "#fff",
-    borderRadius: 5,
-    padding: 15,
-    paddingHorizontal: 20,
-    alignSelf: "center",
-    margin: 20
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    borderWidth: 5,
+    borderColor: "#FFF",
+    marginBottom: 15
+  },
+
+  cancel: {
+    position: "absolute",
+    left: 20,
+    top: 20,
+    backgroundColor: "transparent",
+    color: "#FFF",
+    fontWeight: "600",
+    fontSize: 17
+  },
+
+  next: {
+    position: "absolute",
+    right: 20,
+    top: 20,
+    backgroundColor: "transparent",
+    color: "#FFF",
+    fontWeight: "600",
+    fontSize: 17
   }
 });
--- src/screens/PupilScreen.js
+++ src/screens/PupilScreen.js
@@ -1,16 +1,72 @@
 import React from "react";
-import { Button, View, Text } from "react-native";
+import { Button, View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { RNCamera } from "react-native-camera";
 
 export default class PupilScreen extends React.Component {
+  takePicture = async function() {
+    if (this.camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await this.camera.takePictureAsync(options);
+      console.log(data.uri);
+    }
+  };
+
   render() {
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <View style={styles.container}>
         <Text>Pupil Screen</Text>
         <Button
           title="Go to Balance Screen"
           onPress={() => this.props.navigation.navigate("Balance")}
         />
+        <RNCamera
+          ref={ref => {
+            this.camera = ref;
+          }}
+          style={styles.preview}
+          type={RNCamera.Constants.Type.back}
+          flashMode={RNCamera.Constants.FlashMode.on}
+          permissionDialogTitle={"Permission to use camera"}
+          permissionDialogMessage={
+            "We need your permission to use your camera phone"
+          }
+          onGoogleVisionBarcodesDetected={({ barcodes }) => {
+            console.log(barcodes);
+          }}
+        />
+        <View
+          style={{ flex: 0, flexDirection: "row", justifyContent: "center" }}
+        >
+          <TouchableOpacity
+            onPress={this.takePicture.bind(this)}
+            style={styles.capture}
+          >
+            <Text style={{ fontSize: 14 }}> SNAP </Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "white"
+  },
+  preview: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center"
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: "#fff",
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: "center",
+    margin: 20
+  }
+});
